cedd24c5f572e7fc80baef9443bb6403a775d074
camglab <862786917@qq.com>
2023-04-24 20:22:03 +0800

    shmtest

---
+++ sharedmemory/shm_test.go
@@ -0,0 +1,218 @@
+package sharedmemory
+
+import (
+	"os"
+	"testing"
+
+	"bitbucket.org/avd/go-ipc/mmf"
+	"bitbucket.org/avd/go-ipc/shm"
+	"github.com/pkg/errors"
+)
+
+const SHM_NAME string = "shmtest"
+const SHM_TRUNCATE int64 = 1 << 23
+
+var mo shm.SharedMemoryObject
+
+func createMemoryObject(name string, truncate int64) shm.SharedMemoryObject {
+	shm.DestroyMemoryObject(name)
+	obj, err := shm.NewMemoryObject(name, os.O_CREATE|os.O_EXCL|os.O_RDWR, 0666)
+	if err != nil {
+		panic(errors.Wrap(err, "create memory object"))
+	}
+	if err := obj.Truncate(truncate); err != nil {
+		panic(errors.Wrap(err, "truncate"))
+	}
+	return obj
+}
+
+func initMemoryObject(name string) shm.SharedMemoryObject {
+	obj, err := shm.NewMemoryObject(name, os.O_EXCL|os.O_RDWR, 0666)
+	if err != nil {
+		panic(errors.Wrap(err, "init memory object"))
+	}
+	return obj
+}
+
+func createTestMem() {
+	if mo == nil {
+		mo = createMemoryObject(SHM_NAME, SHM_TRUNCATE)
+	}
+}
+
+func initTestMem() {
+	if mo == nil {
+		mo = initMemoryObject(SHM_NAME)
+	}
+}
+
+func TestSample(t *testing.T) {
+	RWSample()
+}
+
+func TestObject(t *testing.T) {
+	// cleanup previous objects
+	shm.DestroyMemoryObject("obj")
+	// create new object and resize it.
+	obj, err := shm.NewMemoryObject("obj", os.O_CREATE|os.O_EXCL|os.O_RDWR, 0666)
+	if err != nil {
+		panic("new")
+	}
+	defer obj.Close()
+	fd := obj.Fd()
+	t.Logf("FD: %d\n", fd)
+	if err != nil {
+		panic("new")
+	}
+	if err := obj.Truncate(1 << 20); err != nil {
+		panic("truncate")
+	}
+	t.Logf("Size: %d\n", obj.Size())
+}
+
+func TestWrite(t *testing.T) {
+	obj, err := shm.NewMemoryObject("obj", os.O_EXCL|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer obj.Close()
+	t.Logf("FD: %d\n", obj.Fd())
+	t.Logf("Size: %d\n", obj.Size())
+	obj.Size()
+	// create two regions for reading and writing.
+	rwRegion, err := mmf.NewMemoryRegion(obj, mmf.MEM_READWRITE, 0, 1024)
+	if err != nil {
+		panic("new region")
+	}
+	defer rwRegion.Close()
+	writer := mmf.NewMemoryRegionWriter(rwRegion)
+	data := []byte{1, 2, 3, 4, 5, 6, 7, 8}
+	written, err := writer.WriteAt(data, 0)
+	if err != nil || written != len(data) {
+		panic("write")
+	}
+	t.Logf("Write: %d\n", written)
+}
+
+func BenchmarkWrite(b *testing.B) {
+	obj, err := shm.NewMemoryObject("obj", os.O_EXCL|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer obj.Close()
+	b.Logf("FD: %d\n", obj.Fd())
+	b.Logf("Size: %d\n", obj.Size())
+	truncsize := obj.Size()
+	// create two regions for reading and writing.
+	rwRegion, err := mmf.NewMemoryRegion(obj, mmf.MEM_READWRITE, 0, int(truncsize))
+	if err != nil {
+		panic("new region")
+	}
+	defer rwRegion.Close()
+	writer := mmf.NewMemoryRegionWriter(rwRegion)
+	var size int64 = 1 << 12
+	var offset int64 = 0
+	data := make([]byte, size)
+	for i := 0; i < b.N; i++ {
+		written, err := writer.WriteAt(data, offset)
+		if err != nil || written != len(data) {
+			panic("write")
+		}
+		offset = (offset + size) % truncsize
+	}
+}
+
+func TestRead(t *testing.T) {
+	obj, err := shm.NewMemoryObject("obj", os.O_EXCL|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer obj.Close()
+	t.Logf("FD: %d\n", obj.Fd())
+	t.Logf("Size: %d\n", obj.Size())
+
+	roRegion, err := mmf.NewMemoryRegion(obj, mmf.MEM_READ_ONLY, 0, 1024)
+	if err != nil {
+		panic("new region")
+	}
+	defer roRegion.Close()
+	reader := mmf.NewMemoryRegionReader(roRegion)
+	actual := make([]byte, 7)
+	read, err := reader.ReadAt(actual, 1)
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("Read %d\n", read)
+	for _, v := range actual {
+		t.Logf("%d ", v)
+	}
+}
+
+func BenchmarkRead(b *testing.B) {
+	obj, err := shm.NewMemoryObject("obj", os.O_EXCL|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer obj.Close()
+
+	truncsize := obj.Size()
+
+	roRegion, err := mmf.NewMemoryRegion(obj, mmf.MEM_READ_ONLY, 0, int(truncsize))
+	if err != nil {
+		panic("new region")
+	}
+	defer roRegion.Close()
+
+	var size int64 = 1 << 12
+	var offset int64 = 0
+
+	reader := mmf.NewMemoryRegionReader(roRegion)
+	actual := make([]byte, size)
+	for i := 0; i < b.N; i++ {
+		read, err := reader.ReadAt(actual, offset)
+		if err != nil || read != len(actual) {
+			panic(err)
+		}
+		offset = (offset + size) % truncsize
+
+	}
+
+}
+
+func TestWriteString(t *testing.T) {
+	createTestMem()
+	defer mo.Close()
+
+	rwRegion, err := mmf.NewMemoryRegion(mo, mmf.MEM_READWRITE, 0, 2048)
+	if err != nil {
+		panic("new region")
+	}
+
+	defer rwRegion.Close()
+	// writer := mmf.NewMemoryRegionWriter(rwRegion)
+	// defer rwRegion.Close()
+	// rwRegion.
+
+	data := rwRegion.Data()
+	t.Logf("Size of region: %d", len(data))
+	data[0] = 1
+}
+
+func TestReadString(t *testing.T) {
+	initTestMem()
+	defer mo.Close()
+
+	rdRegion, err := mmf.NewMemoryRegion(mo, mmf.MEM_READ_ONLY, 0, 2048)
+	if err != nil {
+		panic("new region")
+	}
+
+	defer rdRegion.Close()
+
+	data := rdRegion.Data()
+	t.Logf("data[1] = %d", data[1])
+}
+
+func TestDestroy(t *testing.T) {
+	shm.DestroyMemoryObject(SHM_NAME)
+}
