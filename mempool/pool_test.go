e94483c9e17944b9637646e329a1e09528b3405b
camglab <caimeng@hashingbot.com>
2024-09-12 17:19:31 +0800

    pool benchmark

--- mempool/pool_test.go
+++ mempool/pool_test.go
@@ -1,114 +1,97 @@
 package mempool
 
-// type iMemPool[T any] interface {
-// 	Free(ptr *T) bool
-// 	New() *T
-// 	Init(int64)
-// }
+import (
+	"runtime"
+	"testing"
+)
 
-// type RawMemPool[T any] struct{}
+type iMemPool[T any] interface {
+	Free(ptr *T) bool
+	New() *T
+	Init(int64)
+}
 
-// func (p *RawMemPool[T]) Free(*T) bool {
-// 	return false
-// }
+type RawMemPool[T any] struct{}
 
-// func (p *RawMemPool[T]) New() *T {
-// 	return new(T)
-// }
+func (p *RawMemPool[T]) Free(*T) bool {
+	return false
+}
 
-// func (p *RawMemPool[T]) Init(int64) {}
+func (p *RawMemPool[T]) New() *T {
+	return new(T)
+}
 
-// type object struct {
-// 	Data [128]byte
-// }
+func (p *RawMemPool[T]) Init(int64) {}
 
-// type PoolTester struct {
-// 	pool     iMemPool[object]
-// 	size     int64
-// 	batch    int64
-// 	parallel int
-// }
+type object struct {
+	Data [128]byte
+}
 
-// func (pt *PoolTester) BenchmarkRandomRW(b *testing.B) {
-// 	var count int
-// 	if pt.parallel > 0 {
-// 		runtime.GOMAXPROCS(pt.parallel)
-// 		count = pt.parallel
-// 	} else {
-// 		count = runtime.GOMAXPROCS(0)
-// 	}
-// 	ch := make(chan int64, count)
-// 	pt.pool.Init(pt.size)
-// 	b.RunParallel(func(pb *testing.PB) {
-// 		allocCount := pt.BenchmarkParallel(pb)
-// 		ch <- int64(allocCount)
-// 	})
-// 	results := make([]int64, count)
-// 	totalAlloc := int64(0)
-// 	for i := 0; i < count; i++ {
-// 		results[i] = <-ch
-// 		totalAlloc += results[i]
-// 	}
-// 	b.Logf("N=%d, AllocRate=%f, %+v", b.N, float64(totalAlloc)/float64(b.N), results)
+type PoolTester struct {
+	pool     iMemPool[object]
+	size     int64
+	batch    int64
+	parallel int
+}
 
-// }
+func (pt *PoolTester) BenchmarkRandomRW(b *testing.B) {
+	var count int
+	if pt.parallel > 0 {
+		runtime.GOMAXPROCS(pt.parallel)
+		count = pt.parallel
+	} else {
+		count = runtime.GOMAXPROCS(0)
+	}
+	ch := make(chan int64, count)
+	pt.pool.Init(pt.size)
+	b.RunParallel(func(pb *testing.PB) {
+		allocCount := pt.BenchmarkParallel(pb)
+		ch <- int64(allocCount)
+	})
+	results := make([]int64, count)
+	totalAlloc := int64(0)
+	for i := 0; i < count; i++ {
+		results[i] = <-ch
+		totalAlloc += results[i]
+	}
+	b.Logf("N=%d, AllocRate=%f, %+v", b.N, float64(totalAlloc)/float64(b.N), results)
 
-// func (pt *PoolTester) BenchmarkParallel(b *testing.PB) int {
-// 	array := make([]*object, pt.batch)
-// 	var allocCount int = 0
-// 	for b.Next() {
-// 		i := rand.Int63n(pt.batch)
-// 		if array[i] == nil {
-// 			array[i] = pt.pool.New()
-// 			allocCount++
-// 		} else {
-// 			pt.pool.Free(array[i])
-// 			array[i] = nil
-// 		}
+}
 
-// 	}
-// 	return allocCount
-// }
+func (pt *PoolTester) BenchmarkParallel(b *testing.PB) int {
+	array := make([]*object, pt.batch)
+	var allocCount int = 0
+	var i int64 = 0
+	for b.Next() {
+		i = (i + 1) % pt.batch
+		if array[i] == nil {
+			array[i] = pt.pool.New()
+			allocCount++
+		} else {
+			pt.pool.Free(array[i])
+			array[i] = nil
+		}
 
-// func BenchmarkMemPoolRW(b *testing.B) {
-// 	pt := &PoolTester{
-// 		pool:     &MemPool[object]{},
-// 		size:     1 << 16,
-// 		batch:    1000,
-// 		parallel: 2,
-// 	}
-// 	pt.BenchmarkRandomRW(b)
-// }
+	}
+	return allocCount
+}
 
-// func BenchmarkRawPoolRW(b *testing.B) {
-// 	pt := &PoolTester{
-// 		pool:     &RawMemPool[object]{},
-// 		size:     1 << 16,
-// 		batch:    1000,
-// 		parallel: 2,
-// 	}
-// 	pt.BenchmarkRandomRW(b)
-// }
+func BenchmarkMemPoolRW(b *testing.B) {
+	pt := &PoolTester{
+		pool:     &MemPool[object]{},
+		size:     1 << 16,
+		batch:    1 << 12,
+		parallel: 4,
+	}
+	pt.BenchmarkRandomRW(b)
+}
 
-// func TestNew(t *testing.T) {
-// 	mp := MemPool[object]{}
-// 	t.Logf("head: %d", mp.cache.header)
-// 	pt := &PoolTester{
-// 		pool:     &mp,
-// 		size:     1 << 16,
-// 		batch:    1000,
-// 		parallel: 2,
-// 	}
-// 	pt.pool.Init(pt.size)
-// 	ptr := pt.pool.New()
-// 	if ptr == nil {
-// 		t.Errorf("new -> nil")
-// 	}
-
-// 	if pt.pool.Free(ptr) {
-// 		t.Logf("Free ok")
-// 	} else {
-// 		t.Logf("idx: %d", mp.cache.getIndex(ptr))
-// 		t.Logf("Free failed")
-// 	}
-// }
+func BenchmarkRawPoolRW(b *testing.B) {
+	pt := &PoolTester{
+		pool:     &RawMemPool[object]{},
+		size:     1 << 16,
+		batch:    1 << 12,
+		parallel: 4,
+	}
+	pt.BenchmarkRandomRW(b)
+}
