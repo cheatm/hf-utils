93f4f8b8136d1a5ac40d3e2ab6f41c5c439c22ff
camglab <caimeng@hashingbot.com>
2024-09-18 15:46:43 +0800

    remove hist logger

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -2,7 +2,6 @@ package mempool
 
 import (
 	"fmt"
-	"sync/atomic"
 )
 
 type IQueue interface {
@@ -14,18 +13,13 @@ type IQueue interface {
 type MemPool[T any] struct {
 	queue casQueue
 	cache bitmapCache[T]
-	pops  []int64
-	pushs []int64
 }
 
 func (m *MemPool[T]) Init(size int64) {
 	m.cache.init(size)
 	m.queue.Init(size + 1)
-	m.pops = make([]int64, size)
-	m.pushs = make([]int64, size)
 	for i := int64(0); i < size; i++ {
 		m.queue.Push(i)
-		atomic.AddInt64(&m.pushs[i], 1)
 	}
 }
 
@@ -36,14 +30,11 @@ func (m *MemPool[T]) New() *T {
 		if m.cache.tag[idx].Load() {
 
 			panic(fmt.Sprintf(
-				"cache[%d] not recycled, q{r:%d, w:%d} pushed: %d, popped: %d",
+				"cache[%d] not recycled, q{r:%d, w:%d}",
 				idx, r, w,
-				atomic.LoadInt64(&m.pushs[idx]),
-				atomic.LoadInt64(&m.pops[idx]),
 			))
 		}
 		m.cache.tag[idx].Store(true)
-		atomic.AddInt64(&m.pops[idx], 1)
 		return &m.cache.cache[idx]
 	}
 	return nil
@@ -54,7 +45,6 @@ func (m *MemPool[T]) Free(ptr *T) bool {
 	if idx < m.cache.size {
 		if m.cache.tag[idx].CompareAndSwap(true, false) {
 			result := m.queue.Push(idx)
-			atomic.AddInt64(&m.pushs[idx], 1)
 			return result
 		}
 	}
